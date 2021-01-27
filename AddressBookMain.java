0f69c876127b4666b78e60eb11265d1ea15ccf4a
Suraj-92 <suraj.temkar.92@gmail.com>
2021-01-27 09:43:24 +0530

    Add New Person Details From Console done

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,26 +1,47 @@
-public class AddressBookMain{
+import java.util.*;
 
- String firstName;
-    String lastName;
-    String address;
-    String city;
-    String state;
-    int zipCode;
-    String phoneNumber;
+class AddressBookMain {
+	static Person readDataFromConsole() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Details of Person");
+		System.out.println("Enter the first name");
+		String firstName = sc.next();
+		System.out.println("Enter the Last name");
+		String lastName = sc.next();
+		System.out.println("Enter the Address");
+		String address = sc.next();
+		System.out.println("Enter the City");
+		String city = sc.next();
+		System.out.println("Enter the State");
+		String state = sc.next();
+		System.out.println("Enter the ZipCode");
+		int zipCode = sc.nextInt();
+		System.out.println("Enter the contact number...");
+		String phoneNumber = sc.next();
 
-   AddressBookMain(String firstName, String lastName, String address, String city, String state, int zipCode,String phoneNumber)
-   {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.address = address;
-      this.city = city;
-      this.state = state;
-      this.zipCode = zipCode;
-      this.phoneNumber = phoneNumber;
-   }
-
-public static void main(String[] args)
-		{
-			AddressBookMain ab = new AddressBookMain("Suraj","Temkar","Chiplun","Ratnagiri","Maharastra",415605,"9503076240");
-		}
+		return new Person(firstName, lastName, address, city, state, zipCode, phoneNumber);
 	}
+static void menu() {
+		Person person = null;
+		int choice;
+		do {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter ur choice press 1 For Add person press 2 For Edit person 3 For the Delete 4 For Display ");
+			choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				person = readDataFromConsole();
+				break;
+			default:
+				System.out.println("Enter numer from 1 to 1");
+			}
+		} while (choice<1);
+	}
+
+public static void main(String[] args) {
+//		 Person person = new Person("Suraj", "Temkar", "Chiplun", "Ratngiri",
+//				"Maharastra", 342345, "4578965123");
+			Person person = readDataFromConsole();
+			menu();
+	}
+}
