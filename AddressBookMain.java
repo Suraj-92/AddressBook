dd9ac31999fac900a0eafce0669cef09965d90bc
Suraj-92 <suraj.temkar.92@gmail.com>
2021-01-27 09:17:11 +0530

    Created Person Details....

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,5 +1,26 @@
 public class AddressBookMain{
-	public static void main(String[] args){
-			System.out.println("Welcome to Address Book.....");
+
+ String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    int zipCode;
+    String phoneNumber;
+
+   AddressBookMain(String firstName, String lastName, String address, String city, String state, int zipCode,String phoneNumber)
+   {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.address = address;
+      this.city = city;
+      this.state = state;
+      this.zipCode = zipCode;
+      this.phoneNumber = phoneNumber;
+   }
+
+public static void main(String[] args)
+		{
+			AddressBookMain ab = new AddressBookMain("Suraj","Temkar","Chiplun","Ratnagiri","Maharastra",415605,"9503076240");
 		}
 	}
