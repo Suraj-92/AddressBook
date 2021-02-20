57530de7c68f68f37fd36d8da8baec2c2bff60bc
Suraj-92 <suraj.temkar.92@gmail.com>
2021-02-20 19:33:49 +0530

    Add Multiple AddressBook Each Has Unique Name UC done

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,117 +1,193 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Scanner;
+import java.util.*;
 
 public class AddressBookMain {
-	ArrayList<Person> al = null;
-	AddressBookMain(){
-		al = new ArrayList<>();
-	}
-	  public void  readDataFromConsole() {
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Details of Person");
-		System.out.println("Enter the first name");
-		String firstName = sc.next();
-		System.out.println("Enter the Last name");
-		String lastName = sc.next();
-		System.out.println("Enter the Address");
-		String address = sc.next();
-		System.out.println("Enter the City");
-		String city = sc.next();
-		System.out.println("Enter the State");
-		String state = sc.next();
-		System.out.println("Enter the ZipCode");
-		String zipCode = sc.next();
-		System.out.println("Enter the contact number...");
-		String phoneNumber = sc.next();
-		
-	al.add( new Person ( firstName, lastName, address, city, state, zipCode, phoneNumber ) );
-	}
-	  public void editPersonDetails(Person person) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Address");
-		person.address = sc.next();
-		System.out.println("Enter the City");
-		person.city = sc.next();
-		System.out.println("Enter the State");
-		person.state = sc.next();
-		System.out.println("Enter the ZipCode");
-		person.zipCode = sc.next();
-		System.out.println("Enter the contact number...");
-		person.phoneNumber = sc.next();
-		//return person;
+	Scanner scanner = new Scanner(System.in);
+	Person Person = new Person();
+	LinkedList<Person> linkedList = new LinkedList<Person>();
+	HashMap<String, LinkedList> hashMap = new HashMap<String, LinkedList>();
+	String addressBookName;
 
+	public void createAddressBook() {
+		System.out.println("\nCreate address book ");
+		System.out.println("\nGive name to the AddressBook ");
+		String addressBookName = scanner.next();
+		System.out.println("AddressBook name is :" + addressBookName);
+		
+		if (hashMap.containsKey(addressBookName)) 
+		{
+			System.out.println("\nAddressBook is already exist ,\nDo u want to create another?(y/n)");
+			String ans = scanner.next();
+			if (ans.equalsIgnoreCase("y")) {
+				createAddressBook();
+			}
+		} else {
+			hashMap.put(addressBookName, new LinkedList<Person>());
+		}
 	}
 	
-	public void  deletePersonDetails(Person person) {
-		return;
+	public void addPerson(String addressBookName1) {
+		if (hashMap.containsKey(addressBookName1)) {
+			Person = new Person();
+			
+			System.out.println("\nEnter Fisrt name :");
+			String firstName = scanner.next();
+			if (hashMap.containsKey(firstName)) {		//no duplicate entry allow
+				System.out.println("\nperson name is already exists ,\nDo u want to create another?(y/n)");
+				String ans = scanner.next();
+				if (ans.equalsIgnoreCase("y")) {
+					addPerson(addressBookName1);
+				}
+			}
+			else
+			{
+				hashMap.put(firstName, new LinkedList<Person>());
+			}
+			System.out.println("\nEnter first name :");
+			String firstName2 = scanner.next();
+			Person.setFirstName(firstName2);
+			System.out.println("\nEnter last name :");
+			String lastName = scanner.next();
+			Person.setLastName(lastName);
+			System.out.println("\nEnter address :");
+			String address = scanner.next();
+			Person.setAddress(address);
+			System.out.println("\nEnter city name :");
+			String city = scanner.next();
+			Person.setCity(city);
+			System.out.println("\nEnter state name :");
+			String state = scanner.next();
+			Person.setState(state);
+			System.out.println("\nEnter zip code :");
+			String zip = scanner.next();
+			Person.setZipCode(zip);
+			System.out.println("\nEnter phone number :");
+			String phone = scanner.next();
+			Person.setPhoneNumber(phone);	
+			
+			if (hashMap.get(addressBookName1) == null) {
+				hashMap.put(addressBookName1, new LinkedList<Person>());
+			}
+			hashMap.get(addressBookName1).add(Person);
+			linkedList = hashMap.get(addressBookName1);
+			for (int i = 0; i < linkedList.size(); i++) {
+				Person p1 = linkedList.get(i);
+				System.out.println("Person name is :" + p1.getFirstName());
+				p1.display();
+			}
+		} else {
+			System.out.println("\n AddressBook is not exists ,\nYou first create AddressBook ");
+			createAddressBook();
+		}			
+	}
+	
+	public void deletePerson() {
+		System.out.println("\nEnter the name of addressBook which you want to delete ....");
+		String deleteName = scanner.next();
+		System.out.println("\nEnter the firstname of user which record you want to delete.... ");
+		String first = scanner.next();
+		LinkedList<Person> linkedList = hashMap.get(deleteName);
+
+		if (linkedList != null) {
+			for (int i = 0; i < linkedList.size(); i++) {
+				Person p1 = linkedList.get(i);
+				if (p1.getFirstName().equalsIgnoreCase(first)) {
+					linkedList.remove(i);
+					System.out.println("Deleted SuccessFully...");
+					System.out.println("\n" + linkedList);
+				}
+			}
+			System.out.println(linkedList.remove(deleteName));
+		} else {
+			System.out.println("No Details available");
+		}
+		for (Person per : linkedList) {
+			System.out.println(per);
+		}
+		System.out.println("\n" + hashMap);		
 	}
 
-	static void menu() {
-		Person person = null;
-		AddressBookMain ab = new AddressBookMain();
-		int choice;
-		do {
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter ur choice \n 1: Add  \n 2: Edit  \n 3: Delete \n 4: Display ");
-			choice = sc.nextInt();
-			switch (choice) {
-			case 1:
-				ab.readDataFromConsole();
-				break;
-			case 2:
-
-				System.out.println("Enter the person Name to edit details..");
-				String firstName = sc.next();
-				ArrayList<Person> personlist = ab.al;
-				for(int i = 0; i < ab.al.size(); i++)
+	public void editPerson() {
+		System.out.println("\nEnter the name of addressBook which you want to update ....");
+		String updateName = scanner.next();
+		System.out.println("\nEnter Name :");
+		String first = scanner.next();
+		boolean flag = hashMap.containsKey(updateName);
+		if (flag) {
+			LinkedList<Person> linkedList = hashMap.get(updateName);
+			for (int i = 0; i < linkedList.size(); i++) {
+				Person p1 = linkedList.get(i);
+				if (p1.getFirstName().equalsIgnoreCase(first)) 
 				{
-				if (personlist.get(i).firstName.equals(firstName))
-				{
-					Person personDetail = personlist.get(i);
-
-					ab.editPersonDetails(personDetail);
-				} else 
-				{
-					System.out.println(firstName + " is not exists ");
-					}
+					System.out.println("\nU can update now   ");
+					System.out.println("\nEnter first name :");
+					String firstName = scanner.next();
+					Person.setFirstName(firstName);
+					System.out.println("\nEnter last name :");
+					String lastName = scanner.next();
+					Person.setLastName(lastName);
+					System.out.println("\nEnter address :");
+					String address = scanner.next();
+					Person.setAddress(address);
+					System.out.println("\nEnter city name :");
+					String city = scanner.next();
+					Person.setCity(city);
+					System.out.println("\nEnter state name :");
+					String state = scanner.next();
+					Person.setState(state);
+					System.out.println("\nEnter zip code :");
+					String zip = scanner.next();
+					Person.setZipCode(zip);
+					System.out.println("\nEnter phone number :");
+					String phone = scanner.next();
+					Person.setPhoneNumber(phone);
+					System.out.println("\nUpdated SuccessFully....");
 				}
+			}
+		}
+		linkedList = hashMap.get(updateName);
+		for (int i = 0; i < linkedList.size(); i++) {
+			Person p1 = linkedList.get(i);
+			System.out.println("Person name is :" + p1.getFirstName());
+			p1.display();
+		}	
+	}
+	
+	public static void menu() {
+		AddressBookMain ab = new AddressBookMain();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("---MenuBar---");
+		String choice;
+		do {
+			System.out.println("\nchoices are :\n1. create new AddressBook \n2. Add Person \n3. Delete Person \n4. Update Person .");
+			System.out.println("\nEnter your choice");
+			int ch = sc.nextInt();
+			switch(ch)
+			{
+			case 1 :
+				ab.createAddressBook();
+				break;
+			
+			case 2 :
+				System.out.println("Give name of the addressBook");
+				String addressBookName1 = sc.next();
+				ab.addPerson(addressBookName1);
 				break;
 			case 3:
-				
-				System.out.println("Enter the Person name to Delete the Details");
-				String firstName1 = sc.next();
-				ArrayList<Person> personlist1 = ab.al;
-				for(int i = 0; i < ab.al.size(); i++)
-				{
-				if (personlist1.get(i).firstName.equals(firstName1))
-				{
-					personlist1.remove(i);
-					System.out.println("Deleted Successfully...");				
-				}
-				else 
-					{
-					System.out.println(firstName1 + " is not exists ");
-					}
-				}
+				ab.deletePerson();
 				break;
-			
 			case 4:
-				System.out.println(ab.al);
+				ab.editPerson();
 				break;
-			
-			default:
-				System.out.println("Enter numer from 1 to 4");
 			}
-		} while (choice<5);
+			System.out.println("\nDo You Want To continue? y Or n");
+			choice = sc.next();
+		}while(choice.equalsIgnoreCase("y"));
+		if(choice.equalsIgnoreCase("n"))
+		{
+			System.out.println("Thank You....");
+		}
 	}
-
 	public static void main(String[] args) {
-//		 Person person1 = new Person("Suraj", "Temkar", "Chiplun", "Ratngiri",
-//		 "Maharastra", "415605", "4578965123");
-		// Person person = readDataFromConsole();
-			menu();
-	}
+		menu();
+	}	
 }
--- Person.java
+++ Person.java
@@ -1,27 +1,85 @@
-public class Person {
-	public String firstName;
+public class Person {public String firstName;
 	public String lastName;
 	public String address;
 	public String city;
 	public String state;
 	public String zipCode;
 	public String phoneNumber;
-	
-	Person(String firstName, String lastName, String address, String city, String state, String zipCode,String phoneNumber)
-	{
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.address = address;
-		this.city = city;
-		this.state = state;
-		this.zipCode = zipCode;
-		this.phoneNumber = phoneNumber;
-	}
-	@Override
-	public String toString() {
-		return "personInfo [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city="
-				+ city + ", state=" + state + ", zipcode=" + zipCode + ", phoneNumber=" + phoneNumber + "]";
-	}
-	
-	
+
+public String getFirstName() {
+	return firstName;
+}
+
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+
+public String getLastName() {
+	return lastName;
+}
+
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+public String getAddress() {
+	return address;
+}
+
+public void setAddress(String address) {
+	this.address = address;
+}
+
+public String getCity() {
+	return city;
+}
+
+public void setCity(String city) {
+	this.city = city;
+}
+
+public String getState() {
+	return state;
+}
+
+public void setState(String state) {
+	this.state = state;
+}
+
+public String getZipCode() {
+	return zipCode;
+}
+
+public void setZipCode(String zipCode) {
+	this.zipCode = zipCode;
+}
+
+public String getPhoneNumber() {
+	return phoneNumber;
+}
+
+public void setPhoneNumber(String phoneNumber) {
+	this.phoneNumber = phoneNumber;
+}
+
+
+@Override
+public String toString() {
+	return ("Name : " + firstName + " " + lastName + "\n" + "Address : " + address + ", " + city + ", " + state
+			+ "(" + zipCode + ")\n" + "Phone No. " + phoneNumber);
+}
+
+public void display() {
+	System.out.println("------------------------ADDRESS BOOK-----------------------");
+
+	System.out.println("Person details are :");
+
+	System.out.println("FirstName is :				 " + firstName);
+	System.out.println("LastName is  : 		 		 " + lastName);
+	System.out.println("Address is   : 				 " + address);
+	System.out.println("City is     : 				 " + city);
+	System.out.println("state is   : 				 " + state);
+	System.out.println("Zipcode is   : 				 " + zipCode);
+	System.out.println("Phone Number is     : 			" + phoneNumber);
+}
 }
